5f2d30e44562443af2f6ebf06426705e21bb7dc0
765335 <765335@elf7it218970.ACDM.DS.SAIT.CA>
2018-10-17 10:18:40 -0600

    Moved getUser to correct folder (User -> UserDB)

--- src/java/database/UserDB.java
+++ src/java/database/UserDB.java
@@ -2,8 +2,13 @@ package database;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import models.User;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UserDB {
 
@@ -35,8 +40,22 @@ public class UserDB {
         return null;
     }
 
-    public User getUser(String username) throws NotesDBException {
-        return null;
+    public User getUser(String username) throws NotesDBException, SQLException{
+        
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+        } catch (SQLException ex) {
+            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        ResultSet users = statement.executeQuery("SELECT * FROM users WHERE username = '" + username + "'");
+        User user = new User (users.getString(1), users.getString(2),users.getString(3),users.getString(4),users.getString(5));
+
+        pool.freeConnection(connection);
+        return (user); 
+    }
     }
 
     public int delete(User user) throws NotesDBException {
