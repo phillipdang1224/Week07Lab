84a74bbe0c9d895cc52bb88ecedcb4f09ccf92fd
758243 <758243@169.254.2.140>
2018-10-17 09:58:09 -0600

    Week07Demo Database completed.

--- src/java/database/NotesDB.sql
+++ src/java/database/NotesDB.sql
@@ -3,5 +3,14 @@ CREATE DATABASE NotesDB;
 
 USE NotesDB;
 
-CREATE TABLE users(username VARCHAR2(20) NOT NULL, password VARCHAR2(20) NOT NULL, firstname VARCHAR2(20) NOT NULL, lastname VARCHAR2(20) NOT NULL, email VARCHAR(40) NOT NULL, CONSTRAINT PK_username PRIMARY KEY (username);
+CREATE TABLE users(
+    username VARCHAR2(20) NOT NULL, 
+    password VARCHAR2(20) NOT NULL, 
+    firstname VARCHAR2(20), 
+    lastname VARCHAR2(20), 
+    email VARCHAR(40), 
+    CONSTRAINT PK_username PRIMARY KEY (username));
+
+INSERT INTO users(username, password)
+VALUES("admin","password");
 COMMIT;
